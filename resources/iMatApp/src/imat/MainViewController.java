fe8fa899bf249037e32e5289e3797b72b48821db
Marten-Granath <115485279+Marten-Granath@users.noreply.github.com>
2023-05-16 11:15:06 +0200

    Click Methods for myButtons

--- resources/iMatApp/src/imat/MainViewController.java
+++ resources/iMatApp/src/imat/MainViewController.java
@@ -11,6 +11,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import se.chalmers.cse.dat216.project.IMatDataHandler;
 
@@ -26,6 +27,12 @@ public class MainViewController implements Initializable {
     private Button myListButton;
     @FXML
     private Button myHistoryButton;
+    @FXML
+    private AnchorPane myAccountWindow;
+    @FXML
+    private AnchorPane myListWindow;
+    @FXML
+    private AnchorPane myHistoryWindow;
 
     IMatDataHandler iMatDataHandler = IMatDataHandler.getInstance();
 
@@ -36,12 +43,20 @@ public class MainViewController implements Initializable {
         //pathLabel.setText(iMatDirectory);
     }
     // Specific Methods
-    public void myAccountButtonPress() throws IOException {
-        change_view("Basket_view.fxml", myAccountButton);
+    private void myAccountButtonClick() throws IOException {
+        myAccountWindow.toFront();
+    }
+    private void myListButtonClick() throws IOException {
+        myListWindow.toFront();
+    }
+    private void myHistoryButtonClick() throws IOException {
+        myHistoryWindow.toFront();
     }
 
+
+
     // General Methods
-    private void change_view(String filename, Control fxml_object) throws IOException {
+    private void changeView(String filename, Control fxml_object) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource(filename));
         Stage stage = (Stage) fxml_object.getScene().getWindow();
         stage.setScene(new Scene(root, 1280, 800));
