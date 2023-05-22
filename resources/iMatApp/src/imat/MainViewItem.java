07032daca7f5595f68ee09a5a82e5452b0deac5e
Marten-Granath <115485279+Marten-Granath@users.noreply.github.com>
2023-05-22 10:48:23 +0200

    Add items to main view code

---
+++ resources/iMatApp/src/imat/MainViewItem.java
@@ -0,0 +1,69 @@
+// This module serves as a template for all cards put into the main view grid
+
+// -- Packages -- //
+package imat;
+
+// -- Imports -- //
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import se.chalmers.cse.dat216.project.Product;
+
+import java.io.IOException;
+
+public class MainViewItem extends AnchorPane {
+
+    private MainViewController parentController;
+    private Product product;
+    private int amount = 0;
+    @FXML
+    private ImageView itemImageView;
+    @FXML
+    private Label itemNameLabel;
+    @FXML
+    private Label itemPriceLabel;
+    @FXML
+    private Label itemAmountLabel;
+
+
+
+    // -- Constructor -- //
+    public MainViewItem(Product product, MainViewController mainViewController){
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main_view_item.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+        this.parentController = mainViewController;
+        this.product = product;
+
+        itemImageView.setImage(parentController.iMatDataHandler.getFXImage(product));
+        itemNameLabel.setText(product.getName());
+        itemPriceLabel.setText(Double.toString(product.getPrice()));
+        itemAmountLabel.setText(Integer.toString(amount));
+    }
+
+    // -- Methods -- //
+
+    public void increase_amount() {
+        amount += 1;
+        itemAmountLabel.setText(Integer.toString(amount));
+    }
+
+    public void decrease_amount() {
+        if(amount>=0){
+            amount -= 1;
+            itemAmountLabel.setText(Integer.toString(amount));
+        }
+    }
+
+}
+
+
+
