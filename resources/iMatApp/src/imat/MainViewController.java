df6a351280a10bd620cb0b57f19ec96d4f88d975
Marten-Granath <115485279+Marten-Granath@users.noreply.github.com>
2023-05-23 23:45:20 +0200

    Updated mainViewController

--- resources/iMatApp/src/imat/MainViewController.java
+++ resources/iMatApp/src/imat/MainViewController.java
@@ -1,139 +1,71 @@
-// This module serves as the main controller of the program
+// This module serves as the controller for the main view
 
 // -- Packages -- //
 package imat;
 
-
 // -- Imports -- //
-import java.io.IOException;
-import java.lang.reflect.Array;
-import java.net.URL;
-import java.util.*;
-
+import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.*;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.effect.ColorAdjust;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
-
+import javafx.scene.layout.Pane;
 import se.chalmers.cse.dat216.project.IMatDataHandler;
-import se.chalmers.cse.dat216.project.Order;
 import se.chalmers.cse.dat216.project.Product;
-import se.chalmers.cse.dat216.project.ShoppingItem;
 
-public class MainViewController implements Initializable {
+import java.io.IOException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.ResourceBundle;
 
-    // -- General Attributes -- //
-
-
+public class MainViewController extends AnchorPane {
+    IMatDataHandler iMatDataHandler = IMatDataHandler.getInstance();
 
     private Map<String, MainViewItem> mainViewItemMap = new HashMap<String, MainViewItem>();
-
     List<Product> productList =  iMatDataHandler.getProducts();
+    private static MainViewController instance = null;
 
-    ColorAdjust enterAdjust = new ColorAdjust(0, 0, -0.1, 0);
-    ColorAdjust exitAdjust = new ColorAdjust(0,0,0, 0);
-    ColorAdjust pressAdjust = new ColorAdjust(0,0,-0.2, 0);
+    // -- Methods -- //
 
+    private static synchronized MainViewController getInstance() {
+        if (instance == null) {
+            instance = new MainViewController();
+        }
+        return instance;
+    }
 
+    public static synchronized Pane getPage() {
+        return getInstance();
+    }
 
+    private MainViewController() {
+        FXMLLoader loader = new FXMLLoader(ApplicationController.class.getResource("main_view.fxml"));
+        loader.setRoot(this);
+        loader.setController(this);
 
-    // -- Specific Attributes -- //
+        try {
+            loader.load();
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
-    @FXML
-    private Label pathLabel;
+        mainViewInitialize();
+    }
 
-    // Navigation Bar Component
-    @FXML
-    private Text navigationBarIMatButton;
-    @FXML
-    private TextField navigationBarSearchBar;
-    @FXML
-    private Button navigationBarHelpButton;
-    @FXML
-    private Button navigationBarAccountButton;
-    @FXML
-    private Button navigationBarBasketButton;
-
-    // Main View
+    // -- FXML objects -- //
     @FXML
     private FlowPane mainViewFlowPane;
-
-    // Account View
     @FXML
-    private Button myAccountButton;
-    @FXML
-    private Button myListButton;
-    @FXML
-    private Button myHistoryButton;
-    @FXML
-    private AnchorPane myAccountWindow;
-    @FXML
-    private AnchorPane myListWindow;
-    @FXML
-    private AnchorPane myHistoryWindow;
-    
-    // Create Account View
-    @FXML
-    private Button createAccountMainButton;
-    @FXML
-    private Button createAccountBackButton;
-    @FXML
-    private Button createAccountShopButton;
-
-    // Login View
-    @FXML
-    private Button loginMainButton;
-    @FXML
-    private Button loginCreateAccountButton;
-    @FXML
-    private Button loginBackButton;
-    @FXML
-    private Button loginShopButton;
-    @FXML
-    private TextField loginTelephoneTextField;
-    @FXML
-    private TextField loginPasswordTextField;
-    @FXML
-    private AnchorPane mainViewItemDetails;
-
-    // Confirmation View
-
-    @FXML
-    private Button confirmationMainButton;
+    private AnchorPane mainViewBasket;
 
 
     // -- Methods -- //
-    public void initialize(URL url, ResourceBundle rb) {
-
-        String iMatDirectory = iMatDataHandler.imatDirectory();
-        mainViewInitialize();
-
-        //pathLabel.setText(iMatDirectory);
-    }
-
-    // Navigation Bar Methods
-
-    public void popUpHelp() {
-        // Pop Up
-    }
-    public void popUpAccount() throws IOException {
-        changeView("imat_app.fxml", navigationBarAccountButton);
-    }
-    public void popUpBasket() {
-        // Pop Up, if not greyed out
-    }
-
-    // Main View Methods
 
     public void mainViewInitialize() {
         populateItemMap();
@@ -141,97 +73,14 @@ public class MainViewController implements Initializable {
     }
     public void populateItemMap() {
         for (Product product: productList) {
-            MainViewItem mainViewItem = new MainViewItem(product, this);
+            MainViewItem mainViewItem = new MainViewItem(product);
             mainViewItemMap.put(product.getName(), mainViewItem);
         }
     }
     public void populateMainView() {
         mainViewFlowPane.getChildren().clear();
-            for (Product product : productList) {
-                mainViewFlowPane.getChildren().add(mainViewItemMap.get(product.getName()));
-            }
+        for (Product product : productList) {
+            mainViewFlowPane.getChildren().add(mainViewItemMap.get(product.getName()));
+        }
     }
-
-    // Account Methods
-    public void myAccountButtonClick() {
-        myAccountWindow.toFront();
-    }
-    public void myListButtonClick() {
-        myListWindow.toFront();
-    }
-    public void myHistoryButtonClick() {
-        myHistoryWindow.toFront();
-    }
-    public void myAccountButtonEnter() {
-        myAccountButton.setEffect(enterAdjust);
-    }
-    public void myAccountButtonExit() {
-        myAccountButton.setEffect(exitAdjust);
-    }
-    public void myAccountButtonPress() {
-        myAccountButton.setEffect(pressAdjust);
-    }
-
-
-
-
-    // Create Account Methods
-
-    public void CreateAccountMainButtonClick() {
-        // Save information
-        // Change view to ShowAccountView
-
-    }
-
-    // Login View Methods
-
-    public void loginMainButtonClick() {
-        // If correct -> Show Account View
-        // If not correct -> Error Message
-    }
-
-    // Confirmation View Methods
-
-
-    // General Methods
-
-    public void toMainView() throws IOException{
-
-    }
-    public void toBasketView() {
-
-    }
-    public void toLoginView() {
-
-    }
-    public void toShowAccountView() {
-
-    }
-    public void toCreateAccountView() {
-
-    }
-    public void toDeliveryView() {
-
-    }
-    public void toPaymentView() {
-
-    }
-    public void toConfirmationView() {
-
-    }
-    public void toAccountView() {
-
-    }
-
-
-
-
-
-    public void changeView(String filename, Control fxml_object) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource(filename));
-        Stage stage = (Stage) fxml_object.getScene().getWindow();
-        stage.setScene(new Scene(root, 1280, 800));
-    }
-
-
 }
