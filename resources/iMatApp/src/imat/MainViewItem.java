c1e39ceaa91be772ef1e6d2f4aa43f0281d913dc
johanneslindq <johanneslindqvist24@gmail.com>
2023-05-24 13:06:26 +0200

    fixed new types for main item

--- resources/iMatApp/src/imat/MainViewItem.java
+++ resources/iMatApp/src/imat/MainViewItem.java
@@ -5,8 +5,10 @@ import imat.MainViewItemDetail;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 import se.chalmers.cse.dat216.project.IMatDataHandler;
 import se.chalmers.cse.dat216.project.Product;
 
@@ -23,9 +25,9 @@ public class MainViewItem extends AnchorPane {
     @FXML
     private Label itemNameLabel;
     @FXML
-    private Label itemPriceLabel;
+    private Text itemPriceLabel;
     @FXML
-    private Label itemAmountLabel;
+    private TextField itemAmountLabel;
 
 
 
